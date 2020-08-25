483945045476e1e5bae3e202e9ffb6a4f46b8b51
rommijares <romuel.mijares@bryanuniversity.edu>
2020-08-25 15:26:50 -0700

    dailyPlanet

---
+++ week2/pNp-ifStatementOlympics/index.js
@@ -0,0 +1,25 @@
+if ( 5>3 ) {
+    console.log ('is greater than')
+}
+
+if ( 'cat' === 3 ) {
+    console.log ('is the length')
+}
+
+if ( 'cat' === 'dog' ) {
+    console.log ('is the same')
+}   else {
+    console.log ('not the same')
+}
+
+var person = {
+    name: 'Bobby',
+    age: 12
+};
+
+if ( person.age >= 18 ) {
+    console.log (person.name + ' ' + 'is allowed')
+} else {
+    console.log (person.name + ' ' + 'is not allowed')
+}
+
